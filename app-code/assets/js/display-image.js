5485929e0065adf7abd09e182e4c1585a77eabb5
nickramen <katherine.nicoleram@gmail.com>
2023-01-13 18:12:34 -0600

    Design changes in index.html
    - Changed html in upload button
    - added 'display-image.js' to display selected image

---
+++ app-code/assets/js/display-image.js
@@ -0,0 +1,10 @@
+const previewImage = (event) => {
+    const imageFiles = event.target.files;
+    const imageFilesLength = imageFiles.length;
+    if (imageFilesLength > 0) {
+        const imageSrc = URL.createObjectURL(imageFiles[0]);
+        const imagePreviewElement = document.querySelector("#preview-selected-image");
+        imagePreviewElement.src = imageSrc;
+        imagePreviewElement.style.display = "block";
+    }
+};
