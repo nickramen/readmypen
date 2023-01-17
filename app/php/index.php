d79b507f1662988849782ce878d94c3ff31f9abe
nickramen <katherine.nicoleram@gmail.com>
2023-01-16 20:07:37 -0600

    Added php files

--- app/php/index.php
+++ app/php/index.php
@@ -1,3 +1,6 @@
+<?php
+    if(isset($_POST['submit']))
+?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -8,23 +11,15 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/assets/css/styles.css" type="text/css" >
-
-        <!-- PyScript -->
-        <!-- <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
-        <script defer src="https://pyscript.net/latest/pyscript.js"></script> -->
+        <link rel="stylesheet" href="../assets/css/styles.css" type="text/css">
 
         <title>Read My Pen!</title>
     </head>
     <body>
-        <!-- <py-env>
-            - cv2
-            - pytesseract
-        </py-env> -->
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12" id="div-title">
-                    <h1>Read My Pen <?php echo "This message is from server side." ?> </h1>
+                    <h1>Read My Pen</h1>
                 </div>
             </div>
             <div class="row">
@@ -73,10 +68,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="assets/js/display-image.js"></script>
+        <script src="../assets/js/display-image.js"></script>
 
-        <!-- Python Scripts -->
-        <!-- <py-script src="../assets/python/text-detection.py"></py-script>
-        <py-script src="../assets/python/save-image.py"></py-script> -->
     </body>
 </html>
