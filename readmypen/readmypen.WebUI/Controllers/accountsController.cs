da32b5f388ad2a24afce15352c69ccc8a534d363
nickramen <katherine.nicoleram@gmail.com>
2023-01-24 23:58:17 -0600

    Added a folder with an ASP.NET Core project
    Decided to try migrating the app to ASP.NET Core

---
+++ readmypen/readmypen.WebUI/Controllers/accountsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace readmypen.WebUI.Controllers
+{
+    public class accountsController : Controller
+    {
+        // GET: accountsController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: accountsController/Details/5
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ readmypen/readmypen.WebUI/wwwroot/js/display-image.js
@@ -0,0 +1,15 @@
+﻿const previewImage = (event) => {
+    const imageFiles = event.target.files;
+    const imageFilesLength = imageFiles.length;
+    if (imageFilesLength > 0) {
+        const imageSrc = URL.createObjectURL(imageFiles[0]);
+        const imagePreviewElement = document.querySelector("#preview-selected-image");
+        imagePreviewElement.src = imageSrc;
+        imagePreviewElement.style.display = "block";
+    }
+};
+
+function closeImage() {
+    const imagePreviewElement = document.querySelector("#preview-selected-image");
+    imagePreviewElement.style.display = "none";
+};
